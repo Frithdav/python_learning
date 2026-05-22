8eb6e12381ad568a419ed8d8a83dca68e4203a24
Frithdav <frithdav@gmail.com>
2026-05-22 06:12:02 +0300

    Loops

---
+++ loops.py
@@ -0,0 +1,10 @@
+#LOOPS
+
+#For Loop
+items = [1,2,3,4,5,"Hi"]
+for item in items:
+    print(f"Round {item}")
+
+ #Range function
+for item in range(10,50,10):
+    print(f"Range Value {item}")
--- math.py
+++ math.py
@@ -1,5 +1,7 @@
 # MATH
 
+import random
+import math
 x = 5
 y = 5.7
 z = 2 + 3J
@@ -12,37 +14,94 @@ print(complex(x, y))
 print(abs(2-10))
 
 # Rounding Numbers
-price = 429.192797 
+price = 429.192797
 print(round(price))
 
-import math
 print(math.floor(price))
 print(math.ceil(price))
 
-import random
 print(random.random())
-print(random.randint(1,10))
+print(random.randint(1, 10))
 
-#Numeric Validation
+# Numeric Validation
 x = 7.0
 print(x.is_integer())
-print(isinstance(x,int))
+print(isinstance(x, int))
 
-print(random.randint(1,100).is_integer())
-
-
-#LOGICAL OPERATOR
-
-  #AND || OR || NOT || IN
-print( not 3>2)
-
-  #"a" in "Data" || 'a' not in 'Data'
-print (3 in [1,2,3])
+print(random.randint(1, 100).is_integer())
 
 
-  #IDF ELSE
+# LOGICAL OPERATOR
+
+# AND || OR || NOT || IN
+print(not 3 > 2)
+
+# "a" in "Data" || 'a' not in 'Data'
+print(3 in [1, 2, 3])
+
+# IF ELSE
 score = 50
-if score >=90:
+if score >= 90:
     print("A")
-else :
+else:
     print("Wololo!")
+
+  # ELIF
+score = 79.999999999999999
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+else:
+    print("D!")
+
+    # Nested  IF
+score = 84
+cat = 25
+if score >= 70:
+    if cat >=25:
+        print("Excellent")
+    else:
+        print("Very Good")
+elif score >= 60:
+    print("Good")
+elif score >= 50:
+    print("Fair")
+else:
+    print("Fail")
+
+email = " Debo@gmail.net"
+password = "3ebo@Gmail.ne7"
+
+# clean email string
+email = email.strip()
+ # email must not be empty check
+if email == "":
+    print("Email cannot be empty!")
+ # email must contain '@' and '.' symbol
+if not ('.' in email and '@' in email):
+    print("Email must contain '.' and '@' symbols. ")
+ # Email must end with .com,.org or .net
+if not email.endswith(('.com', '.org', '.net')):
+    print("Email must end with .com,.org or .net")
+ # Email must contain exactly 1 '@' symbol.
+if email.count('@') != 1:
+    print("Email must contain exactly 1 '@' symbol.")
+ # email must start and end with a letter
+if not(email[0].isalpha() and email[-1].isalpha()):
+    print("Email must start and end with a letter")
+ #password must be atlest 8 characters!
+if len(password) <8:
+    print("password must be atlest 8 characters!")
+ #password can't be same as email.
+if password == email:
+    print("password can't be same as email.")
+ #Password must contain atleast 1 upper annd lower characters
+if not any(p.isupper() for p in password) and any(p.islower() for p in password):
+    print("Password must contain atleast 1 upper annd lower characters")
+ #password must start and end with a digit
+if not(password[0].isdigit() and  password[-1].isdigit):
+    print("password must start and end with a digit")
+else:
+    print("Email is valid")
+
